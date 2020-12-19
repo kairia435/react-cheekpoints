28b1d9f65cba01bbcfb2b62cf8ead365aeefba9f
kairia435 <kairia.mchergui@gmail.com>
2020-12-19 13:22:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import profile from './profile.png'
+
 
 function App() {
-  return (
-    <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-  </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-  );
-}
+   let firstName = "Will";
+   let lastName = "Smith";
+   return (
+     <div>
+        <img src={profile} />  
+       <p class="my-profile" >
+         {firstName} {lastName}
+       </p>
+     </div>
+   );
+  }
 
-export default App;
+  export default App
